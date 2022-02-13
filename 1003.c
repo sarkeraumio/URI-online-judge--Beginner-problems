1c8bc9b525dab32bc565b19a08c5737c6971221d
Aumio Kumar Sarker <87389689+sarkeraumio@users.noreply.github.com>
2022-02-13 12:10:20 +0600

    Add files via upload

---
+++ 1003.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+    int A, B, SOMA;
+
+    while(scanf("%d %d",&A, &B)!= EOF)
+    {
+
+        SOMA= A+ B;
+
+        printf( "SOMA = %d\n", SOMA);
+    }
+
+    return 0;
+}
