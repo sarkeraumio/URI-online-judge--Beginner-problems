5cacb92b61ae914294a8f960b54b0c027db6e661
Aumio Kumar Sarker <87389689+sarkeraumio@users.noreply.github.com>
2022-02-13 21:52:41 +0600

    Add files via upload

---
+++ 1004.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+    int A, B, PROD;
+
+    while(scanf("%d %d",&A, &B)!= EOF)
+    {
+
+        PROD= A*B;
+
+        printf( "PROD = %d\n", PROD);
+    }
+
+    return 0;
+}
+
