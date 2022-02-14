a70cb305ced0d64fb9e35c6aa7702fc217f26383
Aumio Kumar Sarker <87389689+sarkeraumio@users.noreply.github.com>
2022-02-14 19:51:04 +0600

    Add files via upload

---
+++ 1010.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c,d;
+    double e,f,sum;
+
+
+    scanf("%d %d %lf",&a,&b,&e);
+    scanf("%d %d %lf",&c,&d,&f);
+
+    printf("Hiroshi bakchod: R$ %.2lf\n",((b*e) + (d*f)));
+
+
+    return 0;
+}
