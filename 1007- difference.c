8387e35eab6a0f0377abe236fb23a0767b3e57a3
Aumio Kumar Sarker <87389689+sarkeraumio@users.noreply.github.com>
2022-02-14 15:24:57 +0600

    Add files via upload

---
+++ 1007- difference.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+    int A, B, C, D, DIFERENCA;
+
+    while(scanf("%d %d %d %d",&A, &B, &C, &D)!= EOF)
+    {
+
+        DIFERENCA=  (A * B - C * D);
+
+        printf("DIFERENCA = %d\n", DIFERENCA);
+    }
+
+    return 0;
+}
+
